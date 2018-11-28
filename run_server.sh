c200429816fdd038673d854c91c6829113818ad8
Daniel BenDavid <daniel.bendavid@adap.tv>
2018-11-28 13:02:48 -0800

    chapter 7

--- run_server.sh
+++ run_server.sh
@@ -5,4 +5,9 @@ source venv/bin/activate
 export FLASK_APP=microblog.py
 # the following secret key is for mocking purposes only. it should be replaced once deployed to a server.
 export SECRET_KEY=1c3br2ak3r5
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+#export MAIL_USERNAME=<sender_email> defined separately
+#export MAIL_PASSWORD=<sender_email_password> defined separately
 flask run
