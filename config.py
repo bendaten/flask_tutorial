bcc001366ad9128903971b00a8927e8fd7b0b8b0
Daniel BenDavid <daniel.bendavid@adap.tv>
2018-12-04 14:15:40 -0800

    chapter 12

--- config.py
+++ config.py
@@ -17,4 +17,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['bendaten@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 8
